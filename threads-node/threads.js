703dea5cfbefc17faa3869369fb42acdea10418e
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-11-27 14:22:11 -0300

    threads

--- threads-node/threads.js
+++ threads-node/threads.js
@@ -6,4 +6,9 @@ const {
 } = require("worker_threads");
 
 if (isMainThread) {
+  const worker = new Worker(__filename, { workerData: 1 });
+  worker.on("message", (message) => console.log(message));
+} else {
+  const someMath = workerData + 2;
+  parentPort.postMessage(someMath);
 }
